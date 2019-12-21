d0a58fdeb998adb535ad6f0bba8fcf88e62d3ce4
Isaul <rihr_952@hotmail.com>
2019-12-20 23:40:13 -0600

    Punto de venta GepSoft Creado desde Java Descargarlos...!!!

---
+++ src/puntoventa/Conexion.java
@@ -0,0 +1,17 @@
+
+package puntoventa;
+
+/**
+ *
+ * @author Isaul
+ */
+public class Conexion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
