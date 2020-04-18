89e479db05103e4dd75dd44972c6969703b1bac2
aleks <aleks@LAPTOP-0DJLP7K1>
2020-04-18 18:53:38 -0500

    DISEÑO LOGIN PRINCIPAL DISEÑO EDCIONPACIENTES DISEÑO APERTURA DISEÑO CORTE DE CAJA 7DISEÑO GASTOS

--- src/Controladores/Controladorgastos.java
+++ src/Controladores/Controladorgastos.java
@@ -13,22 +13,22 @@ public class Controladorgastos {
      public static  boolean pass=false, pass2=false;
      public static  TikectGasto tikectGastos;
     public static void busquedadegastos(){
-       Modelogastos.mostrartodoslosproductosenexistenciasporbusqueda(Gastos.busquedagastos.getText());
+       Modelogastos.mostrartodoslosproductosenexistenciasporbusqueda(Gastos.busquedagastos1.getText());
     }
     public static void insercionengastos(){
           // ABRE NUEVA VENTANA PARA Registro de Gastos
-          if (Gastos.txtdescripcion.getText().isEmpty() || Gastos.txtmonto.getText().isEmpty()) {
-         Gastos.txtdescripcion.setBackground(Color.red);Gastos.txtmonto.setBackground(Color.red);
+          if (Gastos.txtdescripcionq.getText().isEmpty() || Gastos.txtmonto1.getText().isEmpty()) {
+         Gastos.txtdescripcionq.setBackground(Color.red);Gastos.txtmonto1.setBackground(Color.red);
           } else {             
-                   pass = Controladorventa.validarFormulario(Gastos.txtmonto.getText());
-                   pass2 = validarFormulariotexto(Gastos.txtdescripcion.getText());
+                   pass = Controladorventa.validarFormulario(Gastos.txtmonto1.getText());
+                   pass2 = validarFormulariotexto(Gastos.txtdescripcionq.getText());
                     if (pass && pass2) {
-              Modelogastos.insertarengastos(Gastos.txtdescripcion.getText(), Float.parseFloat(Gastos.txtmonto.getText()));
+              Modelogastos.insertarengastos(Gastos.txtdescripcionq.getText(), Float.parseFloat(Gastos.txtmonto1.getText()));
                          Modelogastos.LlenarTabla(Gastos.jTableGastos); // LLENANDO LA TABLA AL INSERTAR CORRECTAMEBTE
                             tikectGastos = new TikectGasto();
-                            tikectGastos.TikectGasto(0 ,Gastos.txtdescripcion.getText(), String.valueOf(Gastos.txtmonto.getText()));
+                            tikectGastos.TikectGasto(0 ,Gastos.txtdescripcionq.getText(), String.valueOf(Gastos.txtmonto1.getText()));
                               limpiar();
-                              Gastos.txtdescripcion.setBackground(Color.WHITE);Gastos.txtmonto.setBackground(Color.WHITE);
+                              Gastos.txtdescripcionq.setBackground(new Color(206,231,235));Gastos.txtmonto1.setBackground(new Color(206,231,235));
                     }
                 } 
     }
@@ -39,14 +39,14 @@ public class Controladorgastos {
         if (matGastos.matches()) {
             next = true;
         } else {
-          Gastos.txtdescripcion.setBackground(Color.red);
+          Gastos.txtdescripcionq.setBackground(Color.red);
         }
         return next;
     }
      
                    
       public static void limpiar(){     /*====  VACIAR CAMPOS */
-            Gastos.txtdescripcion.setText(null);
-            Gastos.txtmonto.setText(null);
+            Gastos.txtdescripcionq.setText(null);
+            Gastos.txtmonto1.setText(null);
          }  
 }
--- src/si/Gastos.java
+++ src/si/Gastos.java
@@ -21,11 +21,11 @@ Controladorventa.noduplicargastos=true;
         jScrollPane1 = new javax.swing.JScrollPane();
         jTableGastos = new javax.swing.JTable();
         jLabel2 = new javax.swing.JLabel();
-        txtmonto = new javax.swing.JTextField();
-        txtdescripcion = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        busquedagastos = new javax.swing.JTextField();
+        txtdescripcionq = new javax.swing.JTextField();
+        txtmonto1 = new javax.swing.JTextField();
+        busquedagastos1 = new javax.swing.JTextField();
         gastos_btn_back = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
@@ -36,11 +36,11 @@ Controladorventa.noduplicargastos=true;
             }
         });
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBackground(new java.awt.Color(206, 231, 235));
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "GASTOS", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Serif", 1, 36))); // NOI18N
         jPanel1.setLayout(null);
 
-        jPanel2.setBackground(new java.awt.Color(0, 51, 102));
+        jPanel2.setBackground(new java.awt.Color(206, 231, 235));
         jPanel2.setLayout(null);
 
         jTableGastos = new javax.swing.JTable(){
@@ -63,48 +63,66 @@ Controladorventa.noduplicargastos=true;
         jPanel2.add(jScrollPane1);
         jScrollPane1.setBounds(10, 240, 650, 230);
 
+        jLabel2.setBackground(new java.awt.Color(135, 193, 193));
         jLabel2.setFont(new java.awt.Font("Tahoma", 1, 17)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setForeground(new java.awt.Color(135, 193, 193));
         jLabel2.setText("Descripción del Gasto:");
         jPanel2.add(jLabel2);
         jLabel2.setBounds(10, 10, 220, 29);
 
-        txtmonto.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        txtmonto.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                txtmontoKeyReleased(evt);
-            }
-        });
-        jPanel2.add(txtmonto);
-        txtmonto.setBounds(10, 110, 210, 40);
-        jPanel2.add(txtdescripcion);
-        txtdescripcion.setBounds(10, 40, 480, 40);
-
+        jLabel3.setBackground(new java.awt.Color(135, 193, 193));
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 17)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setForeground(new java.awt.Color(135, 193, 193));
         jLabel3.setText("Monto del Gasto:");
         jPanel2.add(jLabel3);
         jLabel3.setBounds(10, 80, 160, 29);
 
-        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(135, 193, 193));
         jLabel4.setText("Busqueda de Gastos");
         jPanel2.add(jLabel4);
         jLabel4.setBounds(10, 150, 250, 40);
 
-        busquedagastos.addKeyListener(new java.awt.event.KeyAdapter() {
+        txtdescripcionq.setBackground(new java.awt.Color(206, 231, 235));
+        txtdescripcionq.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        txtdescripcionq.setForeground(new java.awt.Color(255, 0, 0));
+        txtdescripcionq.setToolTipText("");
+        txtdescripcionq.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 1, true));
+        jPanel2.add(txtdescripcionq);
+        txtdescripcionq.setBounds(10, 40, 460, 40);
+
+        txtmonto1.setBackground(new java.awt.Color(206, 231, 235));
+        txtmonto1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        txtmonto1.setForeground(new java.awt.Color(255, 0, 0));
+        txtmonto1.setToolTipText("");
+        txtmonto1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 1, true));
+        txtmonto1.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyReleased(java.awt.event.KeyEvent evt) {
-                busquedagastosKeyReleased(evt);
+                txtmonto1KeyReleased(evt);
             }
         });
-        jPanel2.add(busquedagastos);
-        busquedagastos.setBounds(10, 200, 220, 30);
+        jPanel2.add(txtmonto1);
+        txtmonto1.setBounds(10, 110, 150, 40);
+
+        busquedagastos1.setBackground(new java.awt.Color(206, 231, 235));
+        busquedagastos1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        busquedagastos1.setForeground(new java.awt.Color(255, 0, 0));
+        busquedagastos1.setToolTipText("");
+        busquedagastos1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 1, true));
+        busquedagastos1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                busquedagastos1KeyReleased(evt);
+            }
+        });
+        jPanel2.add(busquedagastos1);
+        busquedagastos1.setBounds(10, 190, 280, 40);
 
         jPanel1.add(jPanel2);
         jPanel2.setBounds(0, 50, 690, 480);
 
-        gastos_btn_back.setBackground(new java.awt.Color(255, 255, 255));
+        gastos_btn_back.setBackground(new java.awt.Color(206, 201, 235));
         gastos_btn_back.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        gastos_btn_back.setForeground(new java.awt.Color(255, 0, 0));
         gastos_btn_back.setIcon(new javax.swing.ImageIcon(getClass().getResource("/si/IconosJava/salir-flecha-derecha (1).png"))); // NOI18N
         gastos_btn_back.setText("Regresar");
         gastos_btn_back.addActionListener(new java.awt.event.ActionListener() {
@@ -113,7 +131,7 @@ Controladorventa.noduplicargastos=true;
             }
         });
         jPanel1.add(gastos_btn_back);
-        gastos_btn_back.setBounds(530, 0, 150, 40);
+        gastos_btn_back.setBounds(530, 0, 150, 50);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -139,16 +157,16 @@ Controladorventa.noduplicargastos=true;
         dispose();
     }//GEN-LAST:event_gastos_btn_backActionPerformed
 
-    private void busquedagastosKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_busquedagastosKeyReleased
-        Controladorgastos.busquedadegastos();
-    }//GEN-LAST:event_busquedagastosKeyReleased
+    private void busquedagastos1KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_busquedagastos1KeyReleased
+     Controladorgastos.busquedadegastos();
+    }//GEN-LAST:event_busquedagastos1KeyReleased
 
-    private void txtmontoKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtmontoKeyReleased
-        char tecla = evt.getKeyChar();
+    private void txtmonto1KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtmonto1KeyReleased
+          char tecla = evt.getKeyChar();
         if(tecla==KeyEvent.VK_ENTER){
             Controladorgastos.insercionengastos();
         }
-    }//GEN-LAST:event_txtmontoKeyReleased
+    }//GEN-LAST:event_txtmonto1KeyReleased
 
     /**
      * @param args the command line arguments
@@ -189,7 +207,7 @@ Controladorventa.noduplicargastos=true;
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    public static javax.swing.JTextField busquedagastos;
+    public static javax.swing.JTextField busquedagastos1;
     private javax.swing.JButton gastos_btn_back;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -198,7 +216,7 @@ Controladorventa.noduplicargastos=true;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     public static javax.swing.JTable jTableGastos;
-    public static javax.swing.JTextField txtdescripcion;
-    public static javax.swing.JTextField txtmonto;
+    public static javax.swing.JTextField txtdescripcionq;
+    public static javax.swing.JTextField txtmonto1;
     // End of variables declaration//GEN-END:variables
 }
