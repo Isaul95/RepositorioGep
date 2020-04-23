b315d35e207e79850a1e96b3e0c62ebd1678d8c6
COMIMSA <COMIMSA@LAPTOP-A3N257R8>
2020-04-23 11:08:00 -0500

    PDF TERMINADO

--- src/Controladores/Controlador_Report_pdf.java
+++ src/Controladores/Controlador_Report_pdf.java
@@ -27,7 +27,7 @@ import si.nucleo;
 
 public class Controlador_Report_pdf {   
     private final String logotipo = "/Reportes/logoAlk.jpg"; // Logotipo
-    private final String firma = "/Reportes/firma.png";  //  firmafirma.png
+    private final String firma = "/Reportes/firma.png";  //  firma   firma.png
     
      public void Generacion_PDF_client(){                                           
    // ***********************    REPORTE DE USUARIOS    **************************
@@ -42,14 +42,14 @@ public class Controlador_Report_pdf {
             try {
                 Map parametro = new HashMap(); // parameter1 <<-- ESTE PARAMETRO VIENE DESDE EL REPORTE SOLO SE ESTA LLAMANDO 
                 parametro.put("parameter1",Capturar_resultados.id_venta.getText()); 
-                parametro.put("Logo", this.getClass().getResourceAsStream(logotipo));
-                parametro.put("firma", this.getClass().getResourceAsStream(firma));
+                parametro.put("Logo5", this.getClass().getResourceAsStream(logotipo));
+                parametro.put("firma5", this.getClass().getResourceAsStream(firma));
 
                 JasperReport reporte = null;
-                String path = "src/Reportes/report2.jasper";
+                String path = "src/Reportes/report3.jasper";
 
                 //  reporte = (JasperReport) JRLoader.loadObjectFromFile(path);
-                reporte = (JasperReport) JRLoader.loadObject(getClass().getResource("/Reportes/report2.jasper")); // ASI MANDO A LLAMAR LOS REPORTES CON .jasper 
+                reporte = (JasperReport) JRLoader.loadObject(getClass().getResource("/Reportes/report3.jasper")); // ASI MANDO A LLAMAR LOS REPORTES CON .jasper 
                 // ========================= LLENADO DEL REPORTE  ======================  /
                 //  path --> LA RUTA DEL REPORTE
                 //     --> LOS PARAMETROS K SE LE PUEDE ENVIAR ALA REPORTE IN THIS CASE ES NULL y la concion-->(ca) B.D
@@ -70,8 +70,51 @@ public class Controlador_Report_pdf {
   
      } // fin del metodo
      
-   
-      
+     
+     
+/*                   REGISTRO DE DOCUMENTOS DB                               
+      public static boolean registrarArchivoPDF(Connection conn, String file) {
+        FileInputStream input = null;
+        PreparedStatement stmt = null;
+        try {
+            //String sql = "INSERT INTO reportes(archivo) VALUES(?);";
+
+            String sql = "INSERT INTO DOCUMENTO_RESPUESTA_TEMPORAL "
+  + "(ID_RESPUESTA, ID_PREGUNTA, ID_CUESTIONARIO, CVE_FASE, ID_PROYECTO, ANIO, NUMERO_CONVOCATORIA, CVE_TIPO_DOCUMENTO, DOCUMENTO, NOMBRE_DOCUMENTO, USUARIO)"
+  + "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            
+//String sql = "INSERT INTO DOCUMENTO_RESPUESTA_CUEST_SEG";
+            stmt = conn.prepareStatement(sql);
+            input = new FileInputStream(new File(file));
+            
+            
+            
+            stmt.setString(8, CVE_TIPO_DOCUMENTO);  
+            stmt.setBinaryStream(9, input);
+            stmt.setString(10, file);
+            stmt.setString(11, USUARIO);
+           // stmt.setString(13,FECHA_REGISTRO);
+            
+            stmt.executeUpdate();
+            System.out.println("> Archivo '" + file + "' registrado en la base de datos");
+            return true;
+        } catch (FileNotFoundException | SQLException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            try {
+                if (input != null) {
+                    input.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (IOException | SQLException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+        return false;
+    }
+       */
      
      
 }
