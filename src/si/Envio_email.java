3442d3a8e79c42a6106aa4acb4d46b0e6f6ee254
COMIMSA <COMIMSA@LAPTOP-A3N257R8>
2020-04-26 17:52:16 -0500

    activar btn email cuando se genere el pdf

--- src/si/Envio_email.java
+++ src/si/Envio_email.java
@@ -2,13 +2,13 @@
 package si;
 
 import Controladores.Controlador_Report_pdf;
-
-public class Envio_email extends javax.swing.JFrame {
+public class Envio_email extends javax.swing.JFrame { 
  
     public Envio_email() {
         initComponents();
+        this.setLocationRelativeTo(null); // CENTRAR FORMULARIO  
     }
-   
+       
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -18,25 +18,29 @@ public class Envio_email extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         para = new javax.swing.JTextField();
         asunto = new javax.swing.JTextField();
-        ruta = new javax.swing.JTextField();
         emailsend = new javax.swing.JButton();
+        File = new javax.swing.JLabel();
+        send_message = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        regresar = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setUndecorated(true);
 
         jPanel1.setBackground(new java.awt.Color(135, 193, 193));
-        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "APERTURA DE CAJA", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Serif", 1, 36))); // NOI18N
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Envio email", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Serif", 1, 36))); // NOI18N
         jPanel1.setToolTipText("APERTURA DE CAJA");
         jPanel1.setLayout(null);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 17)); // NOI18N
         jLabel1.setText("Para:");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(20, 60, 44, 21);
+        jLabel1.setBounds(20, 60, 50, 30);
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 1, 17)); // NOI18N
         jLabel2.setText("Asunto:");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(20, 100, 67, 21);
+        jLabel2.setBounds(20, 100, 67, 30);
 
         para.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         para.addKeyListener(new java.awt.event.KeyAdapter() {
@@ -62,10 +66,6 @@ public class Envio_email extends javax.swing.JFrame {
         jPanel1.add(asunto);
         asunto.setBounds(110, 100, 460, 30);
 
-        ruta.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
-        jPanel1.add(ruta);
-        ruta.setBounds(170, 140, 350, 31);
-
         emailsend.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
         emailsend.setIcon(new javax.swing.ImageIcon(getClass().getResource("/si/IconosJava/enviar.png"))); // NOI18N
         emailsend.setText("Enviar");
@@ -77,7 +77,36 @@ public class Envio_email extends javax.swing.JFrame {
             }
         });
         jPanel1.add(emailsend);
-        emailsend.setBounds(210, 190, 171, 63);
+        emailsend.setBounds(120, 180, 171, 63);
+
+        File.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        jPanel1.add(File);
+        File.setBounds(250, 140, 320, 30);
+
+        send_message.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
+        send_message.setForeground(new java.awt.Color(204, 0, 0));
+        send_message.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        send_message.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jPanel1.add(send_message);
+        send_message.setBounds(150, 250, 330, 30);
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 17)); // NOI18N
+        jLabel4.setText("Archivo Cargado:");
+        jPanel1.add(jLabel4);
+        jLabel4.setBounds(80, 140, 160, 30);
+
+        regresar.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
+        regresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/si/IconosJava/salir-flecha-derecha (1).png"))); // NOI18N
+        regresar.setText("Regresar");
+        regresar.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        regresar.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        regresar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                regresarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(regresar);
+        regresar.setBounds(350, 180, 171, 63);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -92,8 +121,8 @@ public class Envio_email extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 276, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 290, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -113,8 +142,12 @@ public class Envio_email extends javax.swing.JFrame {
 
     private void emailsendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_emailsendActionPerformed
         Controlador_Report_pdf enviar = new Controlador_Report_pdf();
-        enviar.btn_Envio_email();
+        enviar.btn_Envio_email();             
     }//GEN-LAST:event_emailsendActionPerformed
+
+    private void regresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_regresarActionPerformed
+        dispose();
+    }//GEN-LAST:event_regresarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -152,12 +185,15 @@ public class Envio_email extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public static javax.swing.JLabel File;
     public static javax.swing.JTextField asunto;
-    private javax.swing.JButton emailsend;
+    public static javax.swing.JButton emailsend;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
     public static javax.swing.JPanel jPanel1;
     public static javax.swing.JTextField para;
-    public static javax.swing.JTextField ruta;
+    public static javax.swing.JButton regresar;
+    public static javax.swing.JLabel send_message;
     // End of variables declaration//GEN-END:variables
 }
